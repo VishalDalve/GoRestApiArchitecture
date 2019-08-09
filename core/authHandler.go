67ec36a41aaf15b8b2e33434f338ba42d9a679e6
VishalDalve <vishal.dalve@mail.vinove.com>
2019-08-09 14:24:14 +0530

    Added Registation Login jwt Auth

---
+++ core/authHandler.go
@@ -0,0 +1,66 @@
+package core
+
+import (
+	"encoding/json"
+	"go-restapis/config"
+	"go-restapis/core/httphandler"
+	"go-restapis/model"
+	"go-restapis/service/auth"
+	"go-restapis/service/jwt"
+	"go-restapis/utility"
+	"log"
+	"net/http"
+)
+
+// AuthHandler
+type AuthHadler struct {
+	au auth.AuthService
+	c  *config.Configuration
+}
+
+// NewAuthHandler
+func NewAuthHandler(au auth.AuthService, c *config.Configuration) *AuthHadler {
+	return &AuthHadler{au, c}
+
+}
+
+// Create
+func (h *AuthHadler) Create(w http.ResponseWriter, r *http.Request) {
+	requestUser := new(model.User)
+	defer r.Body.Close()
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestUser)
+	requestUser.SetSaltedPassword(requestUser.Password)
+	requestUser.Role = utility.UserRole
+	err := h.au.Create(r.Context(), requestUser)
+	result := make(map[string]interface{})
+	if err != nil {
+		result = httphandler.NewHTTPError(httphandler.EntityCreationError, http.StatusBadRequest)
+	} else {
+		result["message"] = "Successfully Registered"
+	}
+	httphandler.Response(w, result)
+}
+
+func (h *AuthHadler) Login(w http.ResponseWriter, r *http.Request) {
+	credentials := new(model.Credential)
+	defer r.Body.Close()
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&credentials)
+
+	user, err := h.au.Login(r.Context(), credentials)
+	var result interface{}
+	result = make(map[string]interface{})
+	if err != nil {
+		log.Println(err)
+		result = httphandler.NewHTTPError(httphandler.Unauthorized, http.StatusBadRequest)
+	} else {
+		j := jwt.JwtToken{C: h.c}
+		result, err = j.CreateToken(user.ID.Hex(), user.Role)
+		if err != nil {
+			log.Println(err)
+			result = httphandler.NewHTTPError(httphandler.InternalError, 501)
+		}
+	}
+	httphandler.Response(w, result)
+}
---
+++ core/httphandler/handler.go
@@ -0,0 +1,56 @@
+package httphandler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+)
+
+// Headers set header to request
+func Headers(r http.Handler) http.Handler {
+	headersOk := handlers.AllowedHeaders([]string{"Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "OPTIONS"})
+	return handlers.CORS(headersOk, originsOk, methodsOk)(r)
+}
+
+// Response will return json responce of http
+// This func handle both error a well as success
+func Response(w http.ResponseWriter, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(payload)
+}
+
+// NewHTTPError creates error model that will send as http response
+// if any error occors
+func NewHTTPError(errorCode string, statusCode int) map[string]interface{} {
+
+	m := make(map[string]interface{})
+	m["error"] = errorCode
+	m["error_description"], _ = errorMessage[errorCode]
+	m["code"] = statusCode
+
+	return m
+}
+
+// Error codes
+const (
+	InvalidUserID       = "invalidUserID" // in case userid not exists
+	InternalError       = "internalError" // in case, any internal server error occurs
+	UserNotFound        = "userNotFound"  // if user not found
+	InvalidBindingModel = "invalidBindingModel"
+	EntityCreationError = "entityCreationError"
+	Unauthorized        = "unauthorized" // in case, try to access restricted resource
+)
+
+// Error code with decription
+var errorMessage = map[string]string{
+	"invalidUserID":       "invalid user id",
+	"internalError":       "an internal error occured",
+	"userNotFound":        "user could not be found",
+	"invalidBindingModel": "model could not be bound",
+	"entityCreationError": "could not create entity",
+	"unauthorized":        "an unauthorized access",
+}
